22af4b6f4ec8238ce65329b4e383fd9ad86fb097
Vitor Gabriel <vitorgabriel02007@gmail.com>
2026-05-17 02:58:24 -0300

    Finalização da página!

--- js/main.js
+++ js/main.js
@@ -45,3 +45,13 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+const questions = document.querySelectorAll(".faq-question");
+questions.forEach(question => {
+  question.addEventListener("click", () => {
+
+    const item = question.parentElement;
+    
+    item.classList.toggle("active");
+  });
+});
